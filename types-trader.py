077c80f757faf40e8731d370cb3400cc132f8918
aflyingcougar <aflyingcougar@gmail.com>
2021-08-22 17:52:17 -0500

    can parse types file now

--- types-trader.py
+++ types-trader.py
@@ -1,23 +1,10 @@
 #Python code to illustrate parsing of XML files
 # importing the required modules
 import csv
-import requests
+# import requests
 import xml.etree.ElementTree as ET
 
-def loadRSS():
-
-	# url of rss feed
-	url = 'http://www.hindustantimes.com/rss/topnews/rssfeed.xml'
-
-	# creating HTTP response object from given url
-	resp = requests.get(url)
-
-	# saving the xml file
-	with open('topnewsfeed.xml', 'wb') as f:
-		f.write(resp.content)
-		
-
-def parseXML(xmlfile):
+def parseTypes(xmlfile):
 
 	# create element tree object
 	tree = ET.parse(xmlfile)
@@ -25,58 +12,51 @@ def parseXML(xmlfile):
 	# get root element
 	root = tree.getroot()
 
-	# create empty list for news items
-	newsitems = []
+	# create empty list for item types
+	types = []
 
-	# iterate news items
-	for item in root.findall('./channel/item'):
+	# iterate item types
+	for item in root.findall('./type'):
 
-		# empty news dictionary
-		news = {}
+		typeName = item.attrib['name']
 
-		# iterate child elements of item
-		for child in item:
-
-			# special checking for namespace object content:media
-			if child.tag == '{http://search.yahoo.com/mrss/}content':
-				news['media'] = child.attrib['url']
-			else:
-				news[child.tag] = child.text.encode('utf8')
-
-		# append news dictionary to news items list
-		newsitems.append(news)
+		# append type name item types list
+		types.append(typeName)
 	
-	# return news items list
-	return newsitems
+	# return types list
+	return types
 
 
-def savetoCSV(newsitems, filename):
+def savetoCSV(types, filename):
 
 	# specifying the fields for csv file
-	fields = ['guid', 'title', 'pubDate', 'description', 'link', 'media']
+	fields = ['name']
 
 	# writing to csv file
 	with open(filename, 'w') as csvfile:
 
-		# creating a csv dict writer object
+		# creating a csv type writer object
 		writer = csv.DictWriter(csvfile, fieldnames = fields)
 
 		# writing headers (field names)
 		writer.writeheader()
 
 		# writing data rows
-		writer.writerows(newsitems)
+		writer.writerows(types)
 
 	
 def main():
 	# load rss from web to update existing xml file
-	loadRSS()
+	# loadRSS()
 
-	# parse xml file
-	newsitems = parseXML('topnewsfeed.xml')
+	# parse Types.xml file
+	itemTypes = parseTypes('types.xml')
 
 	# store news items in a csv file
-	savetoCSV(newsitems, 'topnews.csv')
+	# savetoCSV(itemTypes, 'parsedTypes.csv')
+
+	for item in itemTypes:
+		print(item)
 	
 	
 if __name__ == "__main__":
